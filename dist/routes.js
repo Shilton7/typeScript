5b79952d120e42c9cc3bc284a9398a43c31080bf
Shilton <shilton.sistemas@gmail.com>
2020-04-22 20:08:56 -0300

    Typing objects and vectors

--- dist/routes.js
+++ dist/routes.js
@@ -1,6 +1,20 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
+var CreateUser_1 = __importDefault(require("./services/CreateUser"));
 function helloWorld(request, response) {
-    return response.json({ message: 'Hello World' });
+    var user = CreateUser_1.default({
+        email: 'shilton.sis@gmail.com',
+        password: '56464',
+        techs: [
+            'react',
+            'node.js',
+            'vue',
+            { title: 'Javascript', expericence: 107 },
+        ],
+    });
+    return response.json({ user: user });
 }
 exports.helloWorld = helloWorld;
--- src/routes.ts
+++ src/routes.ts
@@ -1,5 +1,17 @@
 import {Request, Response} from 'express';
+import createUser from './services/CreateUser';
 
 export function helloWorld(request: Request, response: Response) {
-  return response.json({ message: 'Hello World' })
+  const user = createUser({
+    email: 'shilton.sis@gmail.com',
+    password: '56464',
+    techs: [
+      'react',
+      'node.js',
+      'vue',
+      { title: 'Javascript', expericence: 107},
+    ],
+  });
+
+  return response.json({ user })
 }
